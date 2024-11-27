84391c0afdf0cc01c3107b72a84bfef7ce85509b
Dhiman004 <123460499+Dhiman004@users.noreply.github.com>
2024-11-27 12:49:27 +0530

    api

---
+++ client/src/UploadsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import UploadList from '../components/UploadList';
+
+const UploadsPage = () => {
+  return (
+    <div className="uploads-page">
+      <h2>Uploads</h2>
+      <UploadList />
+    </div>
+  );
+};
+
+export default UploadsPage;
